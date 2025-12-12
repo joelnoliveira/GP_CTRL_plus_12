f5b2d4b211fb252f22e47763e51029805c0d797e
Tomé Ferreira <uc2021232615@student.uc.pt>
2025-12-12 13:48:09 +0000

    Login + Register Pages started (need components)

--- frontend/react-app/src/pages/Login.jsx
+++ frontend/react-app/src/pages/Login.jsx
@@ -1,5 +1,17 @@
+import { Link } from "react-router-dom";
+
 export default function Login() {
   return (
-	<div></div>
+	<div>
+		<div
+        className="absolute left-0 bottom-0 w-2/5 h-3/5 bg-red-200 -z-2"
+        style={{ clipPath: "polygon(0% 100%, 0% 0%, 100% 100%)" }}
+		></div>
+		<div className="flex py-12 justify-center w-screen h-screen">
+			<Link to="/">
+				<img src="/logo_GP.png" className="w-64 h-64" alt="Logo" />
+			</Link>
+		</div>
+	</div>
   );
 }

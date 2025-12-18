895b4c6f53de12d29ad5188fc08630b7bf7a648c
João Carvalho <uc2024173154@student.uc.pt>
2025-12-18 14:40:18 +0000

    created progress bar, run card and toggle switch components, modified toast to be closed or disappear after 10secs

---
+++ frontend/react-app/src/components/RunCard.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react'
+import ToggleSwitch from "./ToggleSwitch"
+
+import "../styles/components/run_card.css"
+
+const RunCard = (
+    {
+        run_name_id,
+        username,
+        attack_type,
+        date,
+        status,
+        attack_model,
+        target_model,
+    }
+) => {
+    const [isPublic, setIsPublic] = useState(false);
+    const handleIsPublic = (newValue) => {
+        setIsPublic(newValue);
+    }
+
+    const status_class = status === "Ongoing"
+    ? "run-card__header-info__status--ongoing"
+    : status === "Loading" 
+    ? "run-card__header-info__status--loading"
+    : status === "Finished"
+    ? "run-card__header-info__status--finished" 
+    : ""
+
+    return (
+        <div className="run-card__container">
+            <div className="run-card__content__wrapper">
+                <div className="run-card__header">
+                    <div className="run-card__header-top">
+                        <p className="run-card__header__title">{run_name_id}</p>
+
+                        <ToggleSwitch 
+                            checked={isPublic}
+                            onChange={handleIsPublic}
+                        />
+                    </div>
+                    <div className="run-card__header-info">
+
+                        <p className="run-card__header-info__text">
+                            {username}
+                        </p>
+                        <span className="run-card__header-info__divisor"></span>
+                        <p className="run-card__header-info__text">
+                            {attack_type}
+                        </p>
+                        <span className="run-card__header-info__divisor"></span>
+                        <p className="run-card__header-info__text">
+                            {date}
+                        </p>
+                        <span className="run-card__header-info__divisor"></span>
+                        <p className={`run-card__header-info__text ${status_class}`}>
+                            {status === "Loading" ? `${status}...` : status}
+                        </p>
+                    </div>
+                </div>
+
+                <div className="run-card__footer">
+                    <div className="run-card__footer__attack">
+                        <p className="run-card__footer__attack__title">
+                            Attack Model
+                        </p>
+                        <p className="run-card__footer__attack__model-name">
+                            {attack_model}
+                        </p>
+                    </div>
+
+                    <p className="run-card__footer__versus">VS</p>
+                    
+                    <div className="run-card__footer__target">
+                        <p className="run-card__footer__target__title">
+                            Target Model
+                        </p>
+                        <p className="run-card__footer__target__model-name">
+                            {target_model}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default RunCard;
